7608e3f90716d91bddd6416294a7de7ffc2dd3d5
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-04-26 09:39:25 +0530

    user login

--- server/routes/users.js
+++ server/routes/users.js
@@ -46,5 +46,37 @@ router.post("/register", (req, res) => {
 
 });
 
+//================================= Login User =================================//
+router.post("/login", (req, res) => {
+
+    User.findOne({ email: req.body.email }, (err, user) => {
+
+        if (!user)
+            return res.json({
+                loginSuccess: false,
+                message: "Auth failed, email not found"
+            });
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "Wrong password" });
+
+            user.generateToken((err, user) => {
+                if (err) 
+                    return res.status(400).send(err);
+
+                res.cookie("w_authExp", user.tokenExp);
+                res
+                    .cookie("w_auth", user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true, userId: user._id
+                    });
+            });
+
+        });
+    });
+});
 
 module.exports = router;
