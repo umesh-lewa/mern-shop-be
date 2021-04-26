38c06e3ee0d57774b082502a5d9e698f9a12ae2a
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2021-04-26 09:35:43 +0530

    register user

--- server/routes/users.js
+++ server/routes/users.js
@@ -28,7 +28,23 @@ router.get("/auth", auth, (req, res) => {
 
 });
 
+//================================= Register User =================================//
+router.post("/register", (req, res) => {
 
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+
+        if (err) 
+            return res.json({ success: false, err });
+
+        return res.status(200).json({
+            success: true
+        });
+
+    });
+
+});
 
 
 module.exports = router;
